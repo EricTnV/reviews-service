4d71e390a808370c739da7374a83ca877e5dc236
Jonathan Vanasse <jonvanasse@gmail.com>
2021-02-05 21:59:15 -0600

    hopefully surviving the last of several gitches [git glitches?]

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        exclude: [/node_modules/, /sdc/, path.resolve(__dirname, './database/images/'), path.resolve(__dirname, './database/imageLoader.js')],
         use: 'babel-loader',
       },
       {
